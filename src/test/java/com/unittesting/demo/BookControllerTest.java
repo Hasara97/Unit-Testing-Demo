5c6e90f246f25be1a1b60487f2b68573996bc9fe
Hasara <hasarasamson97@gmail.com>
2020-11-14 23:51:39 +0530

    REST Layer Unit Tests

---
+++ src/test/java/com/unittesting/demo/BookControllerTest.java
@@ -0,0 +1,126 @@
+package com.unittesting.demo;
+
+import com.unittesting.demo.controller.BookController;
+import com.unittesting.demo.model.Book;
+import com.unittesting.demo.service.BookService;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = UnitTestingApplication.class)
+public class BookControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @InjectMocks
+    private BookController bookController;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).dispatchOptions(true).build();
+    }
+
+    @Test
+    public void testStoreBook() throws Exception {
+
+        Book book = new Book(123, "Lord of the Rings", "Tolkien", "Fantasy genre book");
+
+        when(bookService.saveBook(book)).thenReturn(book);
+
+        Assert.assertEquals(book, bookController.storeBook(book).getBody());
+
+
+    }
+
+    @Test
+    public void testGetBook() throws Exception {
+
+        Book book = new Book(123, "Lord of the Rings", "Tolkien", "Fantasy genre book");
+
+        when(bookService.getBookById(123)).thenReturn(book);
+
+        mockMvc.perform(get("http://localhost:8080/books/{bookId}", 123).contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("author", Matchers.is("Tolkien")));
+
+
+    }
+
+    @Test
+    public void testGetAllBook() throws Exception {
+
+        Book book1 = new Book(123, "Lord of the Rings", "Tolkien", "Fantasy genre book");
+        Book book2 = new Book(456, "Origin", "Dan Brown", "Thriller/Mystery genre book");
+
+        List<Book> books = new ArrayList<>();
+
+        books.add(book1);
+        books.add(book2);
+
+        when(bookService.getAllBooks()).thenReturn(books);
+
+
+        mockMvc.perform(get("http://localhost:8080/books").contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.*", Matchers.hasSize(2)));
+
+    }
+
+    @Test
+    public void testDeleteBookById() throws Exception {
+
+        Book book = new Book(123, "Lord of the Rings", "Tolkien", "Fantasy genre book");
+
+        bookController.deleteBookById(123);
+
+        verify(bookService).deleteById(123);
+
+    }
+
+    @Test
+    public void testUpdateBook() throws Exception {
+
+        Book book = new Book(123, "Lord of the Rings", "Tolkien", "Fantasy genre book");
+        Book bookUpdated = new Book(123, "Lord of the Rings UPDATED", "Tolkien", "Fantasy genre book");
+
+
+        when(bookService.getBookById(123)).thenReturn(book);
+
+        book.setBookName("Lord of the Rings UPDATED");
+
+        when(bookService.updateBook(123, book)).thenReturn(book);
+
+        Assert.assertEquals(bookUpdated.getBookName(), bookController.updateBook(123, book).getBody().getBookName());
+
+    }
+
+
+}
